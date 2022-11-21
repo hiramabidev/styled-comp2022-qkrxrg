672509971f7ea21e63e0022b49d9096bb632f93e
hiramabidev <102341585+hiramabidev@users.noreply.github.com>
2022-11-21 02:05:17 -0300

    2 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./style.css";
-import {Button} from './components/Button' 
+import {Button} from './components/Button';
+import {BasicCard} from './components/BasicCard';
 
 export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+      <BasicCard/>
       <Button> Aceptar </Button>
     </div>
   );
