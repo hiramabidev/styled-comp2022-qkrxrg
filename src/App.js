87c9a0490ec835c4174707b7aa039b6e59b4bb97
hiramabidev <102341585+hiramabidev@users.noreply.github.com>
2022-11-21 02:21:18 -0300

    1 file modified, 1 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import "./style.css";
-import {Button} from './components/Button';
-import {BasicCard} from './components/BasicCard';
+
+import {Tarjeta} from './components/Tarjeta';
 
 export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
-      <BasicCard/>
-      <Button> Aceptar </Button>
+      <Tarjeta></Tarjeta>
+     
     </div>
   );
 }
--- src/components/Tarjeta.js
+++ src/components/Tarjeta.js
@@ -1,7 +1,7 @@
 import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap';
+import Card from 'react-bootstrap/Card';
 
-function BasicCard() {
+export default function Tarjeta() {
   return (
     <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src="holder.js/100px180" />
@@ -17,5 +17,5 @@ function BasicCard() {
   );
 }
 
-export default BasicCard;
+
 
